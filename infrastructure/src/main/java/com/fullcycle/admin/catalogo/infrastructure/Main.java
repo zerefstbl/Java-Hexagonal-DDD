c9a46d2115725f160b56d9f10822fe158b7474bb
zerefstbl <duduoli11@hotmail.com>
2023-08-18 21:55:52 -0300

    (feat): Added apllication yml'

--- infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/Main.java
+++ infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/Main.java
@@ -3,11 +3,13 @@ package com.fullcycle.admin.catalogo.infrastructure;
 import com.fullcycle.admin.catalogo.infrastructure.configuration.WebServerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.AbstractEnvironment;
 
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
         SpringApplication.run(WebServerConfig.class, args);
     }
 }
